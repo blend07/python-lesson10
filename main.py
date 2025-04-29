77ba5e6b62b5ff394ec3ddff103666c2b290a849
= <=>
2025-04-29 20:48:04 +0200

    2

--- main.py
+++ main.py
@@ -13,3 +13,20 @@ print(summ)
 maxx = max([4 , 56, 23, 53,223, 53,2132])
 
 print(maxx)
+
+import math 
+
+class shape:
+    def area(self):
+        pass
+
+class circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * radius ** 2
+
+Circle = circle(5)
+
+print(Circle.area)
